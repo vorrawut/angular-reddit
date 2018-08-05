134a3997532caac521012532792c1a478310681f
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-08-05 22:45:04 +0700

    create model data of article

---
+++ src/app/models/article.model.ts
@@ -0,0 +1,11 @@
+export class ArticleModel {
+    title: string;
+    link: string;
+    votes: number;
+
+    constructor(title: string, link: string, votes: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes;
+    }
+}
